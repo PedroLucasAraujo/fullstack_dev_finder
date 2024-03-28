8438dc656debd184369fbf37fb4b69f6e6172d40
Pedro Lucas Pinheiro Araujo <pedrolucas.araujo@agenciam3.com>
2024-03-28 20:55:52 -0300

    feat: database, docker, and drizzle setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
-import { ModeToggle } from "@/components/mode-toggle";
+import { ThemeProvider } from "@/src/components/theme-provider";
+import { ModeToggle } from "@/src/components/mode-toggle";
 
 export default function RootLayout({
   children,
