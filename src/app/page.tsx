6ab6ed508620f37c8499fa02f0b6ad5f3deb15a0
Pedro Lucas Pinheiro Araujo <pedrolucas.araujo@agenciam3.com>
2024-03-29 11:52:23 -0300

    feat: create rooms and extend session type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
 import { db } from "../db";
 
 export default async function Home() {
-  const items = await db.query.testing.findMany();
+  const rooms = await db.query.room.findMany();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {items.map((item) => {
-        return <div key={item.id}>{item.name}</div>;
+      {rooms.map((room) => {
+        return (
+          <div key={room.userId}>
+            <h2>{room.name}</h2>
+            <p>{room.description}</p>
+            <a href={room.githubRepo}>{room.githubRepo}</a>
+            <span>{room.language}</span>
+          </div>
+        );
       })}
     </main>
   );
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -4,8 +4,10 @@ import {
   text,
   primaryKey,
   integer,
+  uuid,
 } from "drizzle-orm/pg-core";
 import type { AdapterAccount } from "@auth/core/adapters";
+import { sql } from "drizzle-orm";
 
 export const testing = pgTable("testing", {
   id: text("id").notNull().primaryKey(),
@@ -63,3 +65,19 @@ export const verificationTokens = pgTable(
     compoundKey: primaryKey({ columns: [vt.identifier, vt.token] }),
   })
 );
+
+export const room = pgTable("room", {
+  id: uuid("uuid2")
+    .default(sql`gen_random_uuid()`)
+    .notNull()
+    .primaryKey(),
+  userId: text("userId")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+  name: text("name").notNull(),
+  description: text("description").notNull(),
+  language: text("language").notNull(),
+  githubRepo: text("gitHubRepo"),
+});
+
+export type Room = typeof room.$inferSelect;
